45ddd8888437a8cb8456a21dc51ad25efda94d87
El Pirata Mike <mikefields81@gmail.com>
2018-11-26 12:28:06 -0600

    removing console logs

--- reportBuilder.js
+++ reportBuilder.js
@@ -128,7 +128,6 @@ function initialData () {
 
         // fiscal year ends
         if (YTDMonth == FYEMonth ){
-            console.log(`1`);
             fiscalYE = endDate;
             fiscalYEm1 = endDatePrior;
             fiscalYEm2 = moment(endDatePrior).subtract(1, 'y').format('YYYY-MM-DD');
@@ -136,7 +135,7 @@ function initialData () {
             ytdPeriod = 0;
 
         } else if (YTDMonth < FYEMonth){
-            console.log(`2`);
+
             fiscalYE = moment(new Date(Year-1, FYEMonth-1, endOfMonth[FYEMonth-1])).format('YYYY-MM-DD');
             fiscalYEm1 = moment(fiscalYE).subtract(1, 'y').format('YYYY-MM-DD');
             fiscalYEm2 = moment(fiscalYE).subtract(2, 'y').format('YYYY-MM-DD');
@@ -145,7 +144,6 @@ function initialData () {
 
 
         } else {
-            console.log(`3`);
             fiscalYE = moment (new Date(Year, FYEMonth-1, endOfMonth[FYEMonth-1])).format('YYYY-MM-DD');
             fiscalYEm1 = moment(fiscalYE).subtract(1, 'y').format('YYYY-MM-DD');
             fiscalYEm2 = moment(fiscalYE).subtract(2, 'y').format('YYYY-MM-DD');
